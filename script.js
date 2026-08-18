84f3338c7c224d6d679c3f4fbe24450545c682d3
Jeason Mateus <jeasonmatteusw.@MacBook-Pro-de-Jeason.local>
2026-08-18 15:53:57 -0500

    Ajusta tamaño y posición de imagen en PDF

--- script.js
+++ script.js
@@ -565,7 +565,7 @@ doc.setTextColor(0, 0, 0);
 
 doc.addImage(logo, "PNG", 14, 10, 18, 18);
 
-doc.addImage("jeason.png", "PNG", 14, 270, 18, 6);
+doc.addImage("jeason.png", "PNG", 14, 274, 21, 7);
 
 
 doc.save(
